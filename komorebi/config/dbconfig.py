c2308f7f43450e7a014008e69d68c5df03293458
Roman Baygildin <egdeveloper@mail.ru>
2016-10-17 05:07:43 +0300

    Added 'add user movie'

--- komorebi/config/dbconfig.py
+++ komorebi/config/dbconfig.py
@@ -11,22 +11,15 @@ class DBConfig(Module):
     @singleton
     @provides(DBService)
     def provide_db_service(self):
-        # return DBServiceImpl(
-        #     dialect=SQLDialect.postgres,
-        #     db='komorebi_db',
-        #     host='95.85.24.237',
-        #     user='komorebi_psql',
-        #     password='komorebi95root',
-        #     conn_f=CF()
-        # )
         return DBServiceImpl(
             dialect=SQLDialect.postgres,
-            db='komorebi',
-            host='localhost',
-            user='egdeveloper-psql',
-            password='1Eg9Dev9Root5',
+            db='komorebi_db',
+            host='95.85.24.237',
+            user='komorebi_psql',
+            password='komorebi95root',
             conn_f=CF()
         )
+
 
 class CF(ConnFactory):
 
