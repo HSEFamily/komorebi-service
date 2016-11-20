452f046e3c037fe63db6c1ff8c56abc7c5ce59ac
Destiner <destinerx@gmail.com>
2016-11-20 17:45:02 +0300

    Update service
    Added token retrieval from request parameters
    Passed token to controller methods

--- komorebi/service/user_service.py
+++ komorebi/service/user_service.py
@@ -12,58 +12,75 @@ def setRoutes():
     setMoviesRoutes()
 
 def setUserRoutes():
-    @user_service.route('/user', methods=['POST'])
+    @user_service.route('/user/new', methods=['POST'])
     def new_user():
         user_dict = request.get_json()
         user = controller.save_user(user_dict)
         return 'Success'
 
+    @user_service.route('/user/auth', methods=['POST'])
+    def auth_user():
+        credentials = request.get_json()
+        username = credentials['user_name']
+        password = credentials['password']
+        token = controller.auth_user(username, password)
+        return token   
+
     @user_service.route('/user/<int:id>', methods=['GET'])
     def find_user(id):
-        user = controller.find_user(id)
+        token = request.args.get('sectoken', '')
+        user = controller.find_user(token, id)
         return 'Success'
 
     @user_service.route('/user/<int:id>', methods=['PUT'])
     def update_user(id):
+        token = request.args.get('sectoken', '')
         user_dict = request.get_json()
-        controller.update_user(id, user_dict)
+        controller.update_user(token, id, user_dict)
         return 'Success'
 
     @user_service.route('/user/<int:id>', methods=['DELETE'])
     def delete_user(id):
-        controller.delete_user(id)
+        token = request.args.get('sectoken', '')
+        controller.delete_user(token, id)
         return 'Success'
 
     @user_service.route('/user/<int:user_id>/movie/<int:movie_id>', methods=['POST'])
     def watch_movie(user_id, movie_id):
-        controller.watch_movie(user_id, movie_id)
+        token = request.args.get('sectoken', '')
+        controller.watch_movie(token, user_id, movie_id)
         return 'Success'
 
     @user_service.route('/user/<int:user_id>/movie/<int:movie_id>', methods=['DELETE'])
     def unwatch_movie(user_id, movie_id):
-        controller.unwatch_movie(user_id, movie_id)
+        token = request.args.get('sectoken', '')
+        controller.unwatch_movie(token, user_id, movie_id)
         return 'Success'
 
     @user_service.route('/user/<int:user_id>/movie/<int:movie_id>', methods=['PUT'])
     def set_review(user_id, movie_id):
+        token = request.args.get('sectoken', '')
         review_dict = request.get_json()
-        controller.set_review(user_id, movie_id, review_dict)
+        controller.set_review(token, user_id, movie_id, review_dict)
         return 'Success'
 
     @user_service.route('/user/<int:user_id>/movies', methods=['GET'])
     def get_user_movies(user_id):
-        movies = controller.get_user_movies(user_id)
+        token = request.args.get('sectoken', '')
+        movies = controller.get_user_movies(token, user_id)
         return 'Success'
 
     @user_service.route('/user/<int:user_id>/club', methods=['POST'])
     def create_club(user_id):
+        token = request.args.get('sectoken', '')
         club_dict = request.get_json()
-        controller.create_club(user_id, club_dict)
+        controller.create_club(token, user_id, club_dict)
         return 'Success'
 
     @user_service.route('/user/<int:user_id>/clubs', methods=['GET'])
     def get_user_clubs(user_id):
-        clubs = controller.get_user_clubs(user_id)
+        token = request.args.get('sectoken', '')
+        clubs = controller.get_user_clubs(token, user_id)
         return 'Success'
 
 def setClubRoutes():
@@ -106,11 +123,5 @@ def setMoviesRoutes():
         sq = request.args.get('sq', '')
         movies = controller.find_movies(sq)
         return 'Success'
-
-
-@user_service.route('/user/auth', methods=['POST'])
-def auth_user():
-    # TODO
-    pass
 
 setRoutes()
